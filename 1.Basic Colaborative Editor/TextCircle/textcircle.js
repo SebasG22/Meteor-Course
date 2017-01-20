183c1cc4d31b230ccc90c9a779179f95a04dc473
Sebastian Guevara <rlxsebas@hotmail.com>
2017-01-19 21:12:39 -0500

    Instant Messaging Working

--- 1.Basic Colaborative Editor/TextCircle/textcircle.js
+++ 1.Basic Colaborative Editor/TextCircle/textcircle.js
@@ -1,9 +1,13 @@
 this.Documents = new Mongo.Collection("documents");
+
+/*var myVar = 10;*/
+
 if (Meteor.isClient){
 
 	//Session.set("current_date",new Date());
 
 
+/*
 	Meteor.setInterval(function (){
 		Session.set("current_date",new Date());
 	},1000);
@@ -12,8 +16,12 @@ if (Meteor.isClient){
 	Template.date_display.helpers({
 		current_date:function(){
 			return Session.get("current_date");
+		},
+		myVar:function(){
+			return myVar;
 		}
 	});
+*/
 
 	Template.editor.helpers({
 		docid:function(){
@@ -23,6 +31,14 @@ if (Meteor.isClient){
 			}
 			else{
 				return undefined;
+			}
+		},
+		config:function(){
+			return function (editor){
+				editor.on("change",function(cm_editor,info){
+					console.log(cm_editor.getValue());
+					$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+				})
 			}
 		}
 	});
